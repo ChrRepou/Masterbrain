f3ccd8c8382b95f8f5a9a367af31e79a8ec83b77
Christina-Repou <chr.repou@gmail.com>
2024-09-10 20:24:33 +0200

    adding rank component

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,10 @@
+const Rank = () => {
+  return (
+    <>
+      <div className="white f3">{"Christina, your current rank is..."}</div>
+      <div className="white f1">{"#1"}</div>
+    </>
+  );
+};
+
+export default Rank;
